89cccc2c34014b680ecb263cdeddfdab5ce1ff7d
Jungma1 <mhg3630@naver.com>
2021-03-28 05:42:14 +0900

    Add usePromise custom hook

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import usePromise from '../lib/usePromise';
 import NewsItem from './NewsItem';
 
 const NewsListBlock = styled.div`
@@ -17,42 +18,35 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
 
-  useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' :  `&category=${category}`;
-
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=e54e52a9729542a189d67f24a16fda87`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=e54e52a9729542a189d67f24a16fda87`
+    );
   }, [category]);
 
   // 대기 중일 때
   if (loading) {
-    <NewsListBlock>대기 중...</NewsListBlock>
+    <NewsListBlock>대기 중...</NewsListBlock>;
   }
 
   // 아직 articles 값이 설정되지 않았을 때
-  // if (!articles) {
-  //   return null;
-  // }
+  if (!response) {
+    return null;
+  }
+
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
 
   // articles 값이 유효할 때
+  const { articles } = response.data;
+
   return (
     <NewsListBlock>
-      {articles && articles.map(article => (
+      {articles.map(article => (
         <NewsItem key={article.url} article={article} />
       ))}
     </NewsListBlock>
