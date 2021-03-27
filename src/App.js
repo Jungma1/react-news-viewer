7978c27915fc57e720fe2a9355c3804102693286
Jungma1 <mhg3630@naver.com>
2021-03-27 23:40:00 +0900

    Update change category to router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import { useCallback, useState } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import { Route } from 'react-router';
+import NewsPage from './pages/NewsPage';
 
 function App() {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
   return (
     <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
+      <Route path='/:category?' component={NewsPage} />
     </>
   );
 }
