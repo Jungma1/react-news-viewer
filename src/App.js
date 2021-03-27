e47fa6fc81e85a9d2787195c7b4d3f7bf4a02488
Jungma1 <mhg3630@naver.com>
2021-03-27 22:15:33 +0900

    Add NewsList page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 function App() {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1'
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=e54e52a9729542a189d67f24a16fda87'
       );
       setData(response.data);
     } catch (e) {
@@ -17,18 +18,7 @@ function App() {
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
+    <NewsList />
   );
 }
 
